1277729ddf0c16ab70cafdfe305d748143ec6c63
Matthew Ball <matt.ball@heisencoder.net>
2023-10-13 19:47:34 -0600

    Add pydantic models for spotify API

--- src/analyze.py
+++ src/analyze.py
@@ -5,8 +5,10 @@ This module will parse arguments and perform commands
 
 import argparse
 import pprint
+import json
 
 from src import load_csv
+from src import spotify
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -21,7 +23,10 @@ def main(args: argparse.Namespace):
     """Main entry point for MusicLeague analyzer"""
     directory = args.directory
     all_files = load_csv.load_csvs(directory)
-    pprint.pprint(all_files)
+    submissions = all_files["submissions"]
+    track_uris = [s.SpotifyURI for s in submissions]
+    tracks = spotify.get_tracks(track_uris)
+    pprint.pprint(tracks[0])
 
 
 if __name__ == "__main__":
--- src/spotify.py
+++ src/spotify.py
@@ -1,16 +1,85 @@
 """Interface to spotify API to download track information"""
 
+from typing import Any, List, Optional
+
+from pydantic import BaseModel
+
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-birdy_uri = "spotify:artist:2WX2uTcsvV5OnS0inACecP"
-spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
-results = spotify.artist_albums(birdy_uri, album_type="album")
-albums = results["items"]
-while results["next"]:
-    results = spotify.next(results)
-    albums.extend(results["items"])
+MAX_TRACKS_PER_CALL = 50
 
-for album in albums:
-    print(album["name"])
+
+class ExternalUrls(BaseModel):
+    spotify: str
+
+
+class Artist(BaseModel):
+    external_urls: ExternalUrls
+    href: str
+    id: str
+    name: str
+    type: str
+    uri: str
+
+
+class Image(BaseModel):
+    height: int
+    url: str
+    width: int
+
+
+class Album(BaseModel):
+    album_type: str
+    artists: List[Artist]
+    available_markets: List[str]
+    external_urls: ExternalUrls
+    href: str
+    id: str
+    images: List[Image]
+    name: str
+    release_date: str
+    release_date_precision: str
+    total_tracks: int
+    type: str
+    uri: str
+
+
+class ExternalIds(BaseModel):
+    isrc: str
+
+
+class Track(BaseModel):
+    album: Album
+    artists: List[Artist]
+    available_markets: List[str]
+    disc_number: int
+    duration_ms: int
+    explicit: bool
+    external_ids: ExternalIds
+    external_urls: ExternalUrls
+    href: str
+    id: str
+    is_local: bool
+    name: str
+    popularity: int
+    preview_url: Any
+    track_number: int
+    type: str
+    uri: str
+
+
+def get_tracks(tids: list[str]) -> list[dict]:
+    """Returns Spotify track details for a given list of Spotify URI tracks"""
+    client_credentials_manager = SpotifyClientCredentials()
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+    tracks = []
+
+    for start in range(0, len(tids), MAX_TRACKS_PER_CALL):
+        results = sp.tracks(tids[start : start + MAX_TRACKS_PER_CALL])
+        for track in results["tracks"]:
+            tracks.append(Track.model_validate(track))
+
+    return tracks
