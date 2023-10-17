4ea2f18863a0cf74bd12945e937246c5a765073a
Matthew Ball <matt.ball@heisencoder.net>
2023-10-16 18:14:43 -0600

    create new csv_model as combination of load_csv.py and model.py

--- src/analyze.py
+++ src/analyze.py
@@ -6,7 +6,7 @@ This module will parse arguments and perform commands
 import argparse
 import pprint
 
-from src import load_csv
+from src import csv_model
 from src import spotify
 
 parser = argparse.ArgumentParser()
@@ -22,7 +22,7 @@ parser.add_argument(
 def main(args: argparse.Namespace):
     """Main entry point for MusicLeague analyzer"""
     directory = args.directory
-    all_files = load_csv.load_csvs(directory)
+    all_files = csv_model.load_csvs(directory)
     submissions = all_files.submissions
     track_uris = [s.SpotifyURI for s in submissions]
     tracks = spotify.get_tracks(track_uris)
--- src/csv_model.py
+++ src/csv_model.py
@@ -1,6 +1,9 @@
-"""Pydantic models for JSON responses from Music League Server
-Generated via datamodel-code-generator from actual JSON responses"""
+"""Loads the MusicLeague CSV files into pydantic models.
 
+Pydantic models generated via datamodel-code-generator from actual JSON responses"""
+
+import csv
+import pathlib
 from pydantic import BaseModel
 
 
@@ -58,3 +61,26 @@ class AllFiles(BaseModel):
     rounds: list[Round]
     submissions: list[Submission]
     votes: list[Vote]
+
+
+def load_csv(filename: pathlib.Path) -> list[BaseModel]:
+    """Loads a given CSV filename into a corresponding list of objects"""
+    lines = []
+    model_class = FILE_TO_MODEL_MAP[filename.name]
+    with open(filename, encoding="utf8") as csvfile:
+        reader = csv.DictReader(csvfile)
+        assert reader.fieldnames is not None
+        # remove spaces from headers to make it compatible with the pydantic classes.
+        reader.fieldnames = [x.replace(" ", "") for x in reader.fieldnames]
+        for row in reader:
+            lines.append(model_class(**row))
+    return lines
+
+
+def load_csvs(directory: str) -> AllFiles:
+    """Loads all the MusicLeague CSVs in a given directory"""
+    all_models = {}
+    for basename in FILE_TO_MODEL_MAP:
+        filename = pathlib.Path(directory, basename)
+        all_models[basename.replace(".csv", "")] = load_csv(filename)
+    return AllFiles.model_validate(all_models)
